31b54801ba6dfda4927af0030ea4abf20a49d66a
Greg Smith <crashingwaves@fea.st>
2015-11-18 10:38:02 -0700

    Plays computer moves, takes user input

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,45 @@
-console.log('\'Allo \'Allo!');
+function GameController(){
+    return{
+        compMoves:  [],
+        playerMoves: [],
+        isSounding: false,
+        init: function(){
+            $('.box').click(this.playerMakeSelection.bind(this));
+            this.newCompMove();
+            this.playCompMoves();
+        },
+        playerMakeSelection: function(){
+            var currentId  = event.target.id;
+            this.playerMoves.push(currentId);
+            console.log(this.playerMoves);
+        },
+        playCompMoves: function(){ 
+            
+            var counter     = 0,
+                context     = this,
+                interval    = setInterval(function(){
+                    if(counter === context.compMoves.length){
+                        clearInterval(interval);
+                        context.isSounding = false;
+                        return;
+                    }
+                    context.isSounding  = true;
+                    var targetClass     = '#' + context.compMoves[counter];
+                    context.makeSound(targetClass);                    
+                    counter++;
+                },1000); 
+        },
+        newCompMove: function(){
+            var newNum      = Math.floor(Math.random() * 4),
+                targetClass = '#' + newNum;
+            this.compMoves.push(newNum);
+            this.makeSound(targetClass);
+        },
+        makeSound: function(targetClass){
+           $(targetClass).addClass('sounding');
+           setTimeout(function(){
+            $(targetClass).removeClass('sounding');
+           }, 1000);
+        }
+    }
+}
