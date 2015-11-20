e09a8ccf1c5507d6650f571d20e8a638e4fc2a8b
Greg Smith <crashingwaves@fea.st>
2015-11-20 13:27:31 -0700

    Better styling, feedback implemented. Most of the game works now

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,54 +3,83 @@ function GameController(){
         compMoves:  [],
         playerMoves: [],
         isSounding: false,
+        playerHasMadeSelection: false,
+        playingTimer: null,
+        isPlaying   : false,
         init: function(){
-            $('.box').click(this.playerMakeSelection.bind(this));
-            this.playCompMoves();
+            $('.box').click(this.playerMakeSelection.bind(this));            
+            $('#start').html('Start').click(this.startGame.bind(this));
+        },
+        reset: function(){
+            if(this.playingTimer){
+                clearInterval(this.playingTimer);
+            }
+            this.compMoves      = [];
+            this.playerMoves    = [];
+            $('.feedback').html('Ready?')
+            $('#start').html('Start').off().click(this.startGame.bind(this));
+        },
+        startGame: function(){
+            this.isPlaying = true;
+            this.newCompMove();
+            $('.feedback').html("Here we go!")
+            $('#start').html('Reset').off().click(this.reset.bind(this));
+            this.playingTimer = setTimeout(this.playCompMoves.bind(this), 1800);
         },
         playerMakeSelection: function(){
-            if(this.isSounding){
+            if(this.isSounding || this.playerHasMadeSelection || !this.isPlaying){
                 return;
             }
-            var currentId  = event.target.id;
+            var currentId   = event.target.id;
             this.playerMoves.push(currentId);
             this.makeSound('#' + currentId);
-            if(this.checkLastMove()){
-                console.log("Incorrect!");
-            } else {
-                console.log("Correct!");
+            $(".feedback").html(this.playerMoves.length + '/' + this.compMoves.length)
+            this.checkLastMove();
+            if(this.playerMoves.length === this.compMoves.length){
+                this.playerHasMadeSelection = true;
+                this.playerMoves = [];
+                this.newCompMove();
+                setTimeout(this.playCompMoves.bind(this), 1700);
             }
         },
         playCompMoves: function(counter){ 
-            
-            var counter     = counter || 0,
-                currentPlay = this.compMoves[counter],
-                context     = this;
+            var counter         = counter || 0,
+                currentPlay     = this.compMoves[counter],
+                context         = this;
+                this.isSounding = true;
                 if(counter >= this.compMoves.length){
+                    this.isSounding             = false;
+                    this.playerHasMadeSelection = false;
                     return;
                 } else {
                     this.makeSound('#' + currentPlay);
+                    $('.feedback').html("Level " + this.compMoves.length);
                     setTimeout(function(){
                         context.playCompMoves(counter + 1);
                     }, 1000)
                 }
-                 
         },
         newCompMove: function(){
             var newNum      = Math.floor(Math.random() * 4);
             this.compMoves.push(newNum);
         },
-        makeSound: function(targetClass){
-           $(targetClass).addClass('sounding');
+        makeSound: function(targetId){
+            this.sounding = true;
+           $(targetId).addClass('sounding');
            setTimeout(function(){
-                $(targetClass).removeClass('sounding');
+                $(targetId).removeClass('sounding');
+                this.sounding = false;
            },600)
         },
         checkLastMove: function(){
-            var playerMovesLastIndex    = this.playerMoves.length;
-                console.log(playerMovesLastIndex);
-                if(this.compMoves[playerMovesLastIndex] !== this.playerMoves[playerMovesLastIndex]){
-                    return true;
-                }
+            var indexOfLastPlayerMove    = this.playerMoves.length - 1;
+                
+                if(this.compMoves[indexOfLastPlayerMove] != this.playerMoves[indexOfLastPlayerMove]) {
+                    //try again
+                    this.playerMoves = [];
+                    setTimeout(this.playCompMoves.bind(this), 1000);
+                    $('.feedback').html("Try again!");
+                } 
         }   
     }
 }
